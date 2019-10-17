8644cd71d46ba383d13e4c794805d58fc6cad917
Mitchel Meskes <2115621@talnet.nl>
2019-10-17 10:00:54 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,15 @@
+<?php
+$mensen = array(
+    "Sjonnie de Wiel",
+    "Herman Kaal",
+    "Henk de Steen",
+    "Inge Kerkhoven",
+    "Gert Kruiswijk"
+);
+for ($i = 0; $i < count($mensen); ++$i){
+    echo $mensen[$i] . PHP_EOL;
+}
+
+
+
+?>
